0412d886a6d5175ec7b97ac9508bbeefef2061a4
Paul Williams <paulwilliams@Pauls-MacBook-Pro.local>
2024-03-05 19:13:31 -0500

    utilizing python-dotenv

--- quan_template_project/quan_template_project/settings.py
+++ quan_template_project/quan_template_project/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k5rw9*i=-0no0t-w$2#1#^a*ws$*+)ctlh*bm-p*%k)hu-f)l-'
+SECRET_KEY = os.get.environ('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
